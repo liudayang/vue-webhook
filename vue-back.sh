53f477d18e6d8d2a13a93d9fc50b3a601bff5d25
liuyang <taoguangyanghui111@gmail.com>
2023-09-27 20:47:17 +0800

    msg

--- vue-back.sh
+++ vue-back.sh
@@ -7,7 +7,7 @@ cd $WORK_PATH
 # echo "拉取最新代码"
 # git pull origin master
 # echo "开始构建镜像"
-# docker build -t vue-back:1.0 .
+docker build -t vue-back:1.0 .
 echo "删除旧容器"
 docker stop vue-back-container
 docker rm vue-back-container
