c57936a0b060bd70e850542a9a41a4acd13644f4
liuyang <taoguangyanghui111@gmail.com>
2023-09-27 22:40:49 +0800

    msg

--- vue-front.sh
+++ vue-front.sh
@@ -2,10 +2,10 @@
 WORK_PATH='/usr/projects/vue-front'
 cd $WORK_PATH
 echo "清理代码"
-git reset --hard origin/master
-git clean -f
+# git reset --hard origin/master
+# git clean -f
 echo "拉取最新代码"
-git pull origin master
+git pull
 echo "打包最新代码"
 npm run build
 echo "开始构建镜像"
